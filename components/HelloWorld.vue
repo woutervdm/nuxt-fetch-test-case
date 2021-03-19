ac8e443346f4be6301fe090782f94749eae86abc
Wouter van de Molengraft <wouter@softmedia.nl>
2021-03-19 07:22:49 +0100

    added minimal fetch example

---
+++ components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ data }}
+    <pre>{{ $fetchState }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => ({
+    data: ''
+  }),
+  fetch () {
+    this.data = 'Hello world!'
+  }
+}
+</script>
